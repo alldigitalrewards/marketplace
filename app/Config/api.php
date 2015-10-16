f13f52419367cb31b5dab12cf45d7ce2062380e2
Zech Walden <zech@zewadesign.com>
2015-10-16 11:25:30 -0500

    Update api.php

--- app/Config/api.php
+++ app/Config/api.php
@@ -6,6 +6,6 @@
 return [
     'alias' => 'alldigitalrewards',
     'key' => '6e68b012d3bc897df484300926b976',
-    'host' => 'https://mp3-cbm3.c9.io',
-    'feed_url' => 'https://mp3-cbm3.c9.io/resources/app/products/images/',
+    'host' => 'https://marketplace.iimdevelopment.net',
+    'feed_url' => 'https://marketplace.iimdevelopment.net/resources/app/products/images/',
 ];
