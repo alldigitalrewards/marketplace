aeae39c5214a47eae2287b0b01a127357c5afc95
Zechariah Walden <zech@zewadesign.com>
2016-03-14 22:56:20 -0500

    updates to stripe

---
+++ app/Config/stripe.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Session
+ *
+ * The session can be set as file for file based sessions
+ * or database for database driven sessions.
+ */
+return [
+    "secret_key"      => "sk_test_Gkf4SLISSkr4E0MCKpzysXJu",
+    "publishable_key" => "pk_test_fMhWSg5QUNgfyiQ39ALJZa8y"
+];
